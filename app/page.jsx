d88c4a927222ebb4f30d5493567ddd5daa5065bb
Lauty1904 <lautaro.lucero6b@gmail.com>
2023-08-28 16:07:46 -0300

    arreglos home, works

--- app/page.jsx
+++ app/page.jsx
@@ -17,7 +17,7 @@ function page() {
     >
       <div className={styles.contenedorTitle}>
         <h1 className={styles.title}>Cristian Zaragoza Pintor Profesional</h1>
-        <p className={styles.text}>Pedime tu presupuesto sin cargo</p>
+        <p className={styles.text}>Pedi tu presupuesto sin cargo</p>
         <ContenedorBoton>
           <Link href={'/contact'} className="link">
             <Boton>
@@ -25,17 +25,20 @@ function page() {
             </Boton>
           </Link>
         </ContenedorBoton>
+        <div className={styles.about}>
+        <p className={styles.abtext}>Mi nombre es Cristian. Durante toda mi vida, me he dedicado apasionadamente
+         al mundo de la cartelería y las letras. Junto con mi padre, hemos forjado una trayectoria a lo largo de
+          los años, dejando una marca indeleble en numerosos lugares de Rosario y sus alrededores.
+           Nuestro compromiso y dedicación nos han permitido ser parte del paisaje visual de la ciudad.
+
+En la actualidad, continuamos esta tradición familiar. Acompañado por un grupo de colegas talentosos,
+ trabajamos en equipo para ofrecer una amplia gama de servicios que amplían las posibilidades para nuestros
+  valiosos clientes. Estamos enfocados en brindar soluciones visuales que no solo satisfagan sus necesidades,
+   sino que también superen sus expectativas.</p>
+      </div>
       </div>
 
-      <Link href={'/services'}>
-        <div class={styles.opciones}>
-          <div class={styles.opcion}>CARTELERIA</div>
-          <div class={styles.opcion}>CARPINTERIA</div>
-          <div class={styles.opcion}>SOLDADURA</div>
-          <div class={styles.opcion}>PINTURA</div>
-          <div class={styles.opcion}>ALBAÑILERIA</div>
-        </div>
-      </Link>
+      
     </section>
   );
 }
